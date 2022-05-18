5916a4dd56e4dda2e6251a93847b9a90d52c75ca
Егор Иванов <iwanov.eg@gmail.com>
2022-05-18 16:34:02 +0300

    runes

--- reverse_string.go
+++ reverse_string.go
@@ -2,7 +2,7 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	for i := range input {
-		output += string(input[len(input)-i-1])
+		output += string(rune(input[len(input)-i-1]))
 	}
 	return output
 }
