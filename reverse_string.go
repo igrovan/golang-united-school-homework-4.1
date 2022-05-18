c5354380874a199f3b0c02165ff4e3e201d18cd5
Егор Иванов <iwanov.eg@gmail.com>
2022-05-18 16:43:44 +0300

    implement strings.Builder

--- reverse_string.go
+++ reverse_string.go
@@ -1,8 +1,14 @@
 package reverse_string
 
+import "strings"
+
 func ReverseString(input string) (output string) {
-	for i := range input {
-		output += string(rune(input[len(input)-i-1]))
+	var builder strings.Builder
+	inputRunes := []rune(input)
+
+	for i := len(inputRunes) - 1; i >= 0; i-- {
+		builder.WriteRune(inputRunes[i])
 	}
+	output = builder.String()
 	return output
 }
